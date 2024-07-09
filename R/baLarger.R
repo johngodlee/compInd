5cdde156612a5bda8d2525074def1f2ae3d163aa
John L. Godlee <johngodlee@gmail.com>
2024-07-09 08:57:27 +0100

    Broadened scope of baLarger() to allow any measure of size

--- R/baLarger.R
+++ R/baLarger.R
@@ -1,12 +1,14 @@
 #' Basal area of larger trees - Wykoff et al. 1982
 #'
 #' @param ba vector of basal area of competitor trees
-#' @param focal_ba value basal area of subject tree
+#' @param focal_size size of subject tree
+#' @param size vector of sizes of competitor trees
 #'
 #' @return value of competition index for focal tree
 #' 
-#' @details Returns the sum of basal areas of all trees with basal area larger 
-#' than the focal tree.
+#' @details Returns the sum of basal areas of all trees larger than the focal 
+#' tree. In Wykoff et al. (1982), \code{size} is basal area, though other 
+#' measures of size could be used such as tree height (e.g. Flake et al. 2022).
 #'
 #' @references Wykoff, W. R., Crookston, N. L., Stage, A. R. (1982). User's 
 #' guide to the stand prognosis model. United States Department of Agriculture 
@@ -15,7 +17,7 @@
 #' @export
 #' 
 # Basal area of larger trees - Wykoff et al. 1982
-baLarger <- function(ba, focal_ba) {
-	sum(ba[ba > focal_ba])
+baLarger <- function(ba, focal_size, size) {
+	sum(ba[size > focal_size])
 }
 
